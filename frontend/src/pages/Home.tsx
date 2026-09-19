9a11d9a1b35385da07d5e806be4f49b1f9044948
pdcm5x@umsystem.edu <p.david.corbin@gmail.com>
2026-09-19 13:24:28 -0500

    Added "name" to recipe object

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import RecipeList from '../components/RecipeList.tsx'
 interface Recipe {
     id: string;
+    name: string;
     description: string;
     img: string;
 }
@@ -9,7 +10,8 @@ const Home = () => {
     const recipes: Recipe[] = [
         {
             id: "1",
-            description: "Chicken Alfredo",
+            name: "Chicken Alfredo",
+            description: "Creamy alfredo sauce, Fettuccine pasta, and grilled chicken.",
             img: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FRecipes%2F2023-06-chicken-alfredo-pasta-reshoot%2Fchicken-alfredo-pasta-1-horizontal_e45173-crop"
         }
     ]
