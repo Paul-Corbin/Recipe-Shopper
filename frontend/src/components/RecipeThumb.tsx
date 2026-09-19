18c7a574b142252640ec7845ba702cc22028619f
pdcm5x@umsystem.edu <p.david.corbin@gmail.com>
2026-09-19 13:47:38 -0500

    Began working on adding overlay to recipe items that appear when hovered

--- frontend/src/components/RecipeThumb.tsx
+++ frontend/src/components/RecipeThumb.tsx
@@ -1,7 +1,15 @@
 import '../styles/recipe-thumb.css'
-const RecipeThumb = (props: { img: string | undefined; description: string | undefined; }) => {
-    return (<button className = "recipe-thumb">
-        <img src = {props.img} alt = {props.description} />
+const RecipeThumb = (props: { footer: string | undefined; img: string | undefined; description: string | undefined; name: string | undefined;}) => {
+    return (
+        <button className = "recipe-thumb">
+            <div className="wrapper">
+                <img src = {props.img} alt = {props.name} />
+                <div className="overlay">
+                    <h1 className="name">{props.name}</h1>
+                    <p className="description">{props.description}</p>
+                    <p className="footer">{props.footer}</p>
+            </div>
+        </div>
     </button>)
 }
 export default RecipeThumb;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ interface Recipe {
     id: string;
     name: string;
     description: string;
+    footer: string;
     img: string;
 }
 const Home = () => {
@@ -12,7 +13,8 @@ const Home = () => {
             id: "1",
             name: "Chicken Alfredo",
             description: "Creamy alfredo sauce, Fettuccine pasta, and grilled chicken.",
-            img: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FRecipes%2F2023-06-chicken-alfredo-pasta-reshoot%2Fchicken-alfredo-pasta-1-horizontal_e45173-crop"
+            img: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FRecipes%2F2023-06-chicken-alfredo-pasta-reshoot%2Fchicken-alfredo-pasta-1-horizontal_e45173-crop",
+            footer: "Prep time: 30 mins. Cook time: 30 mins."
         }
     ]
     return (
